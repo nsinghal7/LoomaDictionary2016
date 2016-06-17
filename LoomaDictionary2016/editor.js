16885bd7186c32fa0b5b3b0eb3138c8b9bd8ba08
nsinghal7 <nmsinghal7@gmail.com>
2016-06-17 12:59:00 -0700

    continue formatting editor
    formats the editor again

--- LoomaDictionary2016/editor.js
+++ LoomaDictionary2016/editor.js
@@ -16,34 +16,36 @@ function hideUploadDiv() {
 
 function processPDF() {
 	processing = true;
+	var progress = $("#progressDisplay");
 	$("#uploadPDFDiv").find(".closePopupButton").prop("disabled", true);
 	var file = document.getElementById("pdfInput").files[0];
 	if(file == null || !("name" in file) || !file.name.endsWith(".pdf")) {
-		$("#progressDisplay").text("No file selected or invalid format");
+		progress.text("No file selected or invalid format");
 	}
 	Pdf2TextClass().convertPDF(file, function(page, total) {
-		$("#progressDisplay").text("Converting page " + page + " / " + total);
+		if(page % 5 == 0 || page == total) {
+			progress.text("Converting page " + page + " / " + total);
+		}
 	}, function(text) {
-		$("#progressDisplay").text("Processing text");
+		progress.text("Processing text");
 		var words = findUniqueWordsFromString(text);
 		for(var i = 0; i < words.length; i++) {
-			$("#progressDisplay").text("Creating definitions for word " + i + " / " + words.length);
+			console.log(i)
+			if(i % 100 == 0 || i == words.length - 1) {
+				progress.text("Creating definitions for word " + i + " / " + words.length);
+			}
 			findAndAddDefinitions(words[i]);
 		}
-		$("#progressDisplay").text("Success!");
+		progress.text("Success!");
 		setTimeout(function() {
 			$("#uploadPDFDiv").find(".closePopupButton").prop("disabled", false);
 			processing = false;
 			hideUploadDiv();
-			$("#progressDisplay").text("");
+			progress.text("");
 		}, 1000)
 	});
 }
 
 function findAndAddDefinitions(word) {
 	
-}
-
-function findUniqueWordsFromString(text) {
-	return ["test"];
 }
