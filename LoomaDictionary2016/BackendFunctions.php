12487e23d4ea1655ae0ad34493c71f7c0b9568a4
Colton Conley <coltonconley@gmail.com>
2016-06-22 11:18:30 -0700

    began commenting the steps for each function

--- LoomaDictionary2016/BackendFunctions.php
+++ LoomaDictionary2016/BackendFunctions.php
@@ -1,3 +1,51 @@
 <?php
 
+	function createConnectionToStaging($login){
+		if(verifyLogin($login))
+		{
+			//default is localhost, insert parameters to specify address of database
+			return new MongoClient();
+		}
+		return null;
+	}
+
+	function createConnectionToLooma($login){
+		if(verifyLogin($login))
+		{
+			//default is localhost, insert parameters to specify address of database
+			return new MongoClient();
+		}
+		return null;
+	}
+
+	function createEntry($word, $connection) {
+		
+		//get definition
+		//get translation
+		//get the rw
+		//get the POS
+		//get the date and time
+
+		//put everything into a doc
+
+		// insert the doc into the database
+
+
+		return true;
+	}
+	function readSimplified($args, $connection) {
+		//
+		return array('values' => 'simple');
+	}
+	function readAdvanced($args, $connection) {
+		return array('values' => 'advance');
+	}
+	function publish($login, $connection) {
+		//transfer the data from the staging databse to the Looma database
+		return true;
+	}
+	function updateStaging($new, $connection) {
+		return true;
+	}
+
 ?>
