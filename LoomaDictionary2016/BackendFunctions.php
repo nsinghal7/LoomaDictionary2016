a4d6f73f9d0549586b63d6aaecd201eac00a44e9
Colton Conley <coltonconley@gmail.com>
2016-06-22 15:54:04 -0700

    began working on the create entry function

--- LoomaDictionary2016/BackendFunctions.php
+++ LoomaDictionary2016/BackendFunctions.php
@@ -20,32 +20,60 @@
 
 	function createEntry($word, $connection) {
 		
-		//get definition
-		//get translation
+		//get definition(find api)
+		$def
+
+		//get translation(HARD)
+		$np = 
+		
 		//get the rw
+		$rw =
+
 		//get the POS
+		$POS = 
+		
 		//get the date and time
+		date_default_timezone_set("America/Los_Angeles");
+		$dateCreated = date('m-d-Y') . " at " . date('h:i:sa');
+		
+		//generate random
+		$numDigits = 16;
+		$multiplier = 10 ** $numDigits;
+		$random = rand(0, $multiplier) / $multiplier;
 
 		//put everything into a doc
+		$doc = array(
+		//do we need to specify object id??
+		"ch_id" : "3EN06", //figure out what this is
+		"en" : $word,
+		"rw" : $rw,
+		"np" : $np,
+		"part" : $POS,
+		"def" : $def,
+		"rand" : $random
+			);
 
 		// insert the doc into the database
-
+		$connection->database_name->collection_name->save($doc);
 
 		return true;
 	}
 	function readSimplified($args, $connection) {
-		//
+		//returns the things that are modified
 		return array('values' => 'simple');
 	}
 	function readAdvanced($args, $connection) {
+		//returns all the definitions for the words
 		return array('values' => 'advance');
 	}
 	function publish($login, $connection) {
 		//transfer the data from the staging databse to the Looma database
 		return true;
 	}
+
+	//for backend purposes, this method is equivalent to the createEntry method
 	function updateStaging($new, $connection) {
-		return true;
+		return createEntry($new, $connection);
 	}
 
 ?>
